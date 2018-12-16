f4734d2407dcc15d5b39e6d23901c7df1664a076
Mike Mooney <mikemooney@Mikes-MacBook-Pro-2.local>
2018-12-15 20:04:05 -0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # GIPHYAPP
+This project is an application that queries the GIPHY API for GIFs relating to a specific topic.
+
+This project was designed and built by Mike Mooney using HTML, CSS, Bootstrap, Javascript, and jQuery.
+
+When the app is opened, the page is populated with buttons with the name of a topic within a category. Clicking this button will query the GIPHY API and return 10 static images. The images can be clicked which plays the GIF. 
+
+The user can also add new topics using the submit form. When this is done, a new button is made for the topic, which can be clicked to deliver 10 GIFs as well. 
+
+The rating of the GIF is also showed beneath it when they populate the page.
+
+This project is web-based and can be used with most web browsers.
+
+
---
+++ assets/javascript/logic.js
@@ -0,0 +1,59 @@
+const topics = ["Nissan", "Ford", "Lamborghini", "Mercedes Benz", "BMW", "Toyota", "Bentley", "Mazda"];
+
+function createBtn(item) {
+  return `<button onclick="makeGifs(event);" class="btn1" data-name=${item}>${item}</button>&nbsp&nbsp`;
+}
+
+function renderBtns() {
+  topics.forEach(
+    function (item) {
+      $("#origButtons").append(createBtn(item));
+    }
+  )
+}
+
+function handleApiRes(response) {
+  $("#gifArea").html('')
+
+  response.data.forEach(function (item) {
+    const gifDiv = $("<div>");
+    const p = $("<p>").text("Rating: " + item.rating);
+    const carImage = $("<img>");
+
+    carImage.attr("src", item.images.fixed_height.url);
+    gifDiv.prepend(p);
+    gifDiv.prepend(carImage);
+    $("#gifArea").prepend(gifDiv);
+  })
+}
+
+function makeGifs(event) {
+  const car = event.target.getAttribute('data-name');
+  const queryUrl = `http://api.giphy.com/v1/gifs/search?q=${car}&api_key=qPP6DrSoZK3pIKpDdWO3IOTGcjM3sWYn&limit=10`;
+
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  }).then(handleApiRes)
+}
+
+$(document).ready(function () {
+  renderBtns()
+
+  $("#anything").on("click", function (event) {
+    event.preventDefault();
+    const newCar = $("#form-value").val().trim();
+    $("#origButtons").append(createBtn(newCar));
+  });
+})
+
+$(document).on("click", pausePlayGifs());
+
+function pausePlayGifs() {
+  var state = $(this).attr("data-state");
+   if (state === "still") {
+     $(this).attr("src", $(this).attr("data-animate"));
+     $(this).attr("data-state", "animate");
+   } else {
+     $(this).attr("src", $(this).attr("data-still"));
+     $(this).attr("data-state", "still")}}
